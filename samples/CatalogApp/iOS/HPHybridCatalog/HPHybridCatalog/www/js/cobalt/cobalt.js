c6adc346c5c3dea0488e0f2d6a675200cf4f6c6b
Sébastien Vitard <contact@sebastienvitard.name>
2014-01-20 16:34:48 +0100

    update www content for ios catalog

--- samples/CatalogApp/iOS/HPHybridCatalog/HPHybridCatalog/www/js/cobalt/cobalt.js
+++ samples/CatalogApp/iOS/HPHybridCatalog/HPHybridCatalog/www/js/cobalt/cobalt.js
@@ -418,3 +418,54 @@ var cobalt={
 
 
 };
+cobalt.ios_adapter={
+	//
+	//IOS ADAPTER
+	//
+    pipeline:[], //array of sends waiting to go to native
+    pipelineRunning:false,//bool to knwow if new sends should go to pipe or go to native
+
+	init:function(){
+		cobalt.platform="iOs";
+	},
+	// handle callbacks sent by native side
+    handleCallback:function(json){
+        switch(json.callback){
+            case "callbackSimpleAcquitment":
+                //cobalt.log("received callbackSimpleAcquitment", false)
+                cobalt.adapter.unpipe();
+                if (cobalt.adapter.pipeline.length==0){
+                    cobalt.log('set pipe running=false', false)
+                    cobalt.adapter.pipelineRunning=false;
+                }
+                break;
+	        default:
+			    cobalt.tryToCallCallback(json)
+		    break;
+        }
+    },
+    //send native stuff
+    send:function(obj){
+	    cobalt.log('adding to pipe', false)
+        cobalt.adapter.pipeline.push(obj);
+        if (!cobalt.adapter.pipelineRunning){
+            cobalt.adapter.unpipe()
+        }
+    },
+    //unpipe elements when receiving a ACK from ios.
+    unpipe:function(){
+        cobalt.adapter.pipelineRunning=true;
+        var objToSend=cobalt.adapter.pipeline.shift();
+	    if (objToSend && cobalt.sendingToNative){
+            cobalt.log('----sending : '+JSON.stringify(objToSend), false)
+            document.location.href=encodeURIComponent("h@ploid#k&y"+JSON.stringify(objToSend));
+        }
+    },
+	//default behaviours
+	handleEvent : cobalt.defaultBehaviors.handleEvent,
+	navigateToModale : cobalt.defaultBehaviors.navigateToModale,
+	dismissFromModale : cobalt.defaultBehaviors.dismissFromModale,
+	initStorage : cobalt.defaultBehaviors.initStorage
+
+};
+cobalt.adapter=cobalt.ios_adapter;
