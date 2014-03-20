eaf9986428f7d227ec2f45ea82f8dad7307d9ce5
Haploid <haploid@MacBook-Pro-Antoine-dev-2.local>
2014-03-20 15:01:11 +0100

    added ImageManager stuff

---
+++ sources/iOS/Cobalt/Cobalt/Model/ImageManager.h
@@ -0,0 +1,13 @@
+//
+//  ImageManager.h
+//  Cobalt
+//
+//  Created by Haploid on 20/03/14.
+//  Copyright (c) 2014 Haploid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageManager : NSObject
+
+@end
---
+++ sources/iOS/Cobalt/Cobalt/Model/ImageManager.m
@@ -0,0 +1,13 @@
+//
+//  ImageManager.m
+//  Cobalt
+//
+//  Created by Haploid on 20/03/14.
+//  Copyright (c) 2014 Haploid. All rights reserved.
+//
+
+#import "ImageManager.h"
+
+@implementation ImageManager
+
+@end
