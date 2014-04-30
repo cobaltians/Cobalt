1af7df419ff1554d5aae9c40833c15527e8d22d7
Guillaume <guillaume@haploid.fr>
2014-04-30 09:56:51 +0200

    Starting tu update and translate the catalog

---
+++ samples/CatalogApp/Android/CobaltCatalog/assets/www/js/app.js
@@ -0,0 +1,32 @@
+var app={
+    debug:true,
+    debugInBrowser:true,
+    //debugInLogdiv:true,
+
+
+    /* This code below, combined with the touch module of zepto helps in resolving issues
+       with fastclick on android devices : some devices receive "tap" event before "click" and
+       some don't. ensure only one event is fired in all cases.
+    */
+    touchTimer:null,
+    touch:function(selector, touchHandler, allowDefault){
+        var preventDefault = allowDefault ? false : true;
+        var elem=$(selector)
+        var touchup=function(){
+            $(this).removeClass('touched');
+        };
+        var touching=function(e){
+            if (!$(this).hasClass('touched')){
+                $(this).addClass('touched');
+                clearTimeout(app.touchTimer);
+                app.touchTimer = setTimeout(touchup.bind(this),1000);
+                touchHandler.apply(this,e);
+            }
+            if (preventDefault)
+                return false;
+        }
+        elem.unbind('tap').on('tap',touching);
+        elem.unbind('click').on('click',touching);
+    }
+
+}
