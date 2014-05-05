950c3b690bd7348945005c82a2b545ea11c4b3aa
Sébastien Vitard <contact@sebastienvitard.name>
2014-05-05 11:38:28 +0200

    Delete all use of empty menu in Catalog

--- samples/CatalogApp/Android/CobaltCatalog/src/fr/cobaltians/cobaltcatalog/activities/ZoomHybridActivity.java
+++ samples/CatalogApp/Android/CobaltCatalog/src/fr/cobaltians/cobaltcatalog/activities/ZoomHybridActivity.java
@@ -1,22 +1,13 @@
 package fr.cobaltians.cobaltcatalog.activities;
 
-import android.view.Menu;
 import fr.cobaltians.cobaltcatalog.fragments.ZoomHybridFragment;
+
 import fr.cobaltians.cobalt.activities.HTMLActivity;
 import fr.cobaltians.cobalt.fragments.HTMLFragment;
-import fr.cobaltians.cobaltcatalog.R;
 
 public class ZoomHybridActivity extends HTMLActivity{
 	
-	protected HTMLFragment getFragment(){
-		return new ZoomHybridFragment();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+	protected HTMLFragment getFragment() {
+        return new ZoomHybridFragment();
+    }
 }
