cb051c62696dd1719b11f2c576ac373a1b2f6351
Sébastien Vitard <contact@sebastienvitard.name>
2014-05-07 16:08:47 +0200

    Move setResourcePath in app

--- samples/CatalogApp/Android/CobaltCatalog/src/fr/cobaltians/cobaltcatalog/activities/SimpleHybridActivity.java
+++ samples/CatalogApp/Android/CobaltCatalog/src/fr/cobaltians/cobaltcatalog/activities/SimpleHybridActivity.java
@@ -6,16 +6,7 @@ import fr.cobaltians.cobalt.Cobalt;
 import fr.cobaltians.cobalt.activities.CobaltActivity;
 import fr.cobaltians.cobalt.fragments.CobaltFragment;
 
-import android.os.Bundle;
-
 public class SimpleHybridActivity extends CobaltActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Cobalt.getInstance(this).setResourcePath("www/common/");
-    }
 
     protected CobaltFragment getFragment() {
 		return Cobalt.getInstance(this).getFragmentForController(SimpleHybridFragment.class, "default", "index.html");
