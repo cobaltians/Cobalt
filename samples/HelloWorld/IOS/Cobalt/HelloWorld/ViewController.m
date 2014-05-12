54d3a24fab4304aa7b51a6ee1155a4ccd48db482
Haploid <haploid@MacBook-Pro-Antoine-dev-2.local>
2014-05-12 15:08:53 +0200

    renamed Cobalt projet to HelloWorld

---
+++ samples/HelloWorld/IOS/Cobalt/HelloWorld/ViewController.m
@@ -0,0 +1,17 @@
+//
+//  ViewController.m
+//  Cobalt
+//
+//  Created by Julien Gambier on 20/12/2013.
+//  Copyright (c) 2013 Julien Gambier. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+@end
