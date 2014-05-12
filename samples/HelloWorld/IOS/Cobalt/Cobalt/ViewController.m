e4fd130b9889716369935db051a179e78a520a59
Haploid <haploid@MacBook-Pro-Antoine-dev-2.local>
2014-05-12 12:19:46 +0200

    removed .svn directory + fixed HelloWorld compilation

--- samples/HelloWorld/IOS/Cobalt/Cobalt/ViewController.m
+++ samples/HelloWorld/IOS/Cobalt/Cobalt/ViewController.m
@@ -20,7 +20,7 @@
 {
 
     	// Do any additional setup after loading the view, typically from a nib.
-    self.isPullToRefreshActive=YES;
+    self.isPullToRefreshEnabled=YES;
     self.pageName=@"index.html";
     
     [super viewDidLoad];
