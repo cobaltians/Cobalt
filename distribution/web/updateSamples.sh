303aa0a8938407c5ec28e49a1bb34a9d31f0deb0
ggendre <dzb.rtz@gmail.com>
2014-05-13 15:40:59 +0200

    Renaming helloworld ios and changing path

--- distribution/web/updateSamples.sh
+++ distribution/web/updateSamples.sh
@@ -2,7 +2,7 @@
 
 ##updating Helloworld
 cp Android/* ../../samples/HelloWorld/Android/Hello/assets/www/js/cobalt/
-cp iOs/* ../../samples/HelloWorld/iOs/Cobalt/www/js/cobalt/
+cp iOs/* ../../samples/HelloWorld/iOs/Hello/www/js/cobalt/
 
 ##updating Catalog
 cp Android/* ../../samples/CatalogApp/Android/CobaltCatalog/assets/www/platform
