1b5e7e7dbc138f0ff81e899db683f0b4451a0bfd
Guillaume Gendre <guillaume@MacBook-Pro-de-Guillaume.local>
2014-07-15 16:27:05 +0200

    Adapted geoloc plugin usage into catalog app

--- samples/CatalogApp/iOS/CatalogCobalt/CatalogCobalt/www/common/js/cobalt.geoloc.js
+++ samples/CatalogApp/iOS/CatalogCobalt/CatalogCobalt/www/common/js/cobalt.geoloc.js
@@ -1,27 +1,14 @@
-//move this to core
-cobalt.plugins={
-	enabledPlugins:[],
-	init:function(){		
-		for (var i=0; i<cobalt.plugins.enabledPlugins.length;i++){
-			cobalt.plugins.enabledPlugins[i].init();
-		}
-		
-	}
-
-}
-
-
-//(function(cobalt){
+(function(cobalt){
 	var plugin={
 		name:"geoloc",
-		init:function(){
-			cobalt.log('initing geoloc plugin')			
+		init:function(options){
+			cobalt.log('initing geoloc plugin with options', options)
 			
 			//create shortcuts
-			cobalt.getGeoloc=this.sendEvent;
+			cobalt.getGeoloc=this.getGeoloc;
 			
 		},
-		sendEvent:function(callback){
+        getGeoloc:function(callback){
 			this.callback=callback;
 			cobalt.log('sending getGeoloc call')
 			cobalt.send({ type : "plugin", name:"geoloc"}, callback)
@@ -30,11 +17,8 @@ cobalt.plugins={
 		handleEvent:function(data){
 			cobalt.log('received native getGeoloc call', data)			
 		}
+	};
+	cobalt.plugins.register(plugin);
 	
-	}
+})(cobalt || {});
 
-	//keep this in geoloc plugin
-	cobalt.plugins.enabledPlugins.push(plugin);
-	
-	//})(cobalt || {});
-cobalt.plugins.init();
