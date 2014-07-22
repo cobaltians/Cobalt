cf0fcd888fcf81c2f78bb615d7afa5eb1e542155
Myrdhinn <sebastien.famel@gmail.com>
2014-07-22 10:38:02 +0200

    add class CobaltLocationPlugin

---
+++ sources/Android/Cobalt/src/fr/cobaltians/cobalt/plugin/CobaltLocationPlugin.java
@@ -0,0 +1,102 @@
+/**
+ *
+ * CobaltLocationPlugin
+ * Cobalt
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Cobaltians
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+package fr.cobaltians.cobalt.plugin;
+
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import fr.cobaltians.cobalt.Cobalt;
+import fr.cobaltians.cobalt.fragments.CobaltFragment;
+
+import android.app.Activity;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by sebastienfamel on 16/07/2014.
+ */
+public class CobaltLocationPlugin extends CobaltAbstractPlugin{
+
+	// TAG
+	private static final String TAG = CobaltLocationPlugin.class.getSimpleName();
+
+    private final static String LONGITUDE = "longitude";
+    private final static String LATITUDE = "latitude";
+
+    /***********************************************************************************************************
+     * CONSTRUCTORS
+     ***********************************************************************************************************/
+
+	private CobaltLocationPlugin(Activity activity, CobaltFragment fragment, CobaltPluginManager pluginManager) {
+		super(activity, fragment, pluginManager);
+	}
+	
+	public static CobaltAbstractPlugin getInstance(Activity activity, CobaltFragment fragment, CobaltPluginManager pluginManager) {
+    	if (sInstance == null) {
+    		sInstance = new CobaltLocationPlugin(activity, fragment, pluginManager);
+    	}
+    	
+    	return sInstance;
+    }
+
+
+	/******************************************
+     * OVERRIDEN METHODS
+     ******************************************/
+	
+	@Override
+	public void onMessage(JSONObject message) {
+        LocationManager locationManager = (LocationManager) mActivity.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        try {
+            String callback = message.getString(Cobalt.kJSCallback);
+            if (location != null) {
+                JSONObject resultLocation = new JSONObject();
+                resultLocation.put(LATITUDE, location.getLatitude());
+                resultLocation.put(LONGITUDE, location.getLongitude());
+                mFragment.sendCallback(callback, resultLocation);
+            }
+            else if (location == null) {
+                mFragment.sendCallback(callback, null);
+                if (Cobalt.DEBUG) Log.d(TAG, "location is NULL");
+            }
+            else if (Cobalt.DEBUG) Log.d(TAG, "onMessage: " + message.toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+	}
+}
