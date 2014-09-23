4c06620a4730d880025bb60b730bfb59b3157140
Guillaume Gendre <guillaume@MacBook-Pro-de-Guillaume.local>
2014-09-23 10:48:03 +0200

    IOS Adapter upgrade for IOS7+
    since ios7, no more url change is required to send messages from web to
    native

--- sources/Web/adapters/iOS/adapter.js
+++ sources/Web/adapters/iOS/adapter.js
@@ -3,42 +3,42 @@ cobalt.ios_adapter={
     //IOS ADAPTER
     //
     pipeline:[], //array of sends waiting to go to native
-    pipelineRunning:false,//bool to knwow if new sends should go to pipe or go to native
+    pipelineRunning:false,//bool to know if new sends should go to pipe or go to native
+
+    isBelowIOS7:false,
 
     init:function(){
         cobalt.platform="iOs";
+
+        if (typeof CobaltWebCommunicationClass === "undefined"){
+            cobalt.log('Warning : CobaltWebCommunicationClass undefined. We probably are below ios7.')
+            cobalt.adapter.isBelowIOS7 = true;
+        }else{
+            cobalt.adapter.isBelowIOS7 = false;
+        }
     },
     // handle callbacks sent by native side
     handleCallback:function(json){
-        switch(json.callback){
-            case "callbackSimpleAcquitment":
-                cobalt.divLog("received message acquitment")
-                cobalt.adapter.unpipe();
-                if (cobalt.adapter.pipeline.length==0){
-                    cobalt.divLog('end of ios message stack')
-                    cobalt.adapter.pipelineRunning=false;
-                }
-                break;
-            default:
-                cobalt.tryToCallCallback(json)
-                break;
+        if (cobalt.adapter.isBelowIOS7){
+            cobalt.adapter.ios6.handleCallback(json);
+        }else{
+            cobalt.defaultBehaviors.handleCallback(json);
         }
     },
     //send native stuff
     send:function(obj){
-        cobalt.divLog('adding to ios message stack', obj)
-        cobalt.adapter.pipeline.push(obj);
-        if (!cobalt.adapter.pipelineRunning){
-            cobalt.adapter.unpipe()
-        }
-    },
-    //unpipe elements when receiving a ACK from ios.
-    unpipe:function(){
-        cobalt.adapter.pipelineRunning=true;
-        var objToSend=cobalt.adapter.pipeline.shift();
-        if (objToSend && !cobalt.debugInBrowser){
-            cobalt.divLog('sending',objToSend)
-            document.location.href=encodeURIComponent("cob@l7#k&y"+JSON.stringify(objToSend));
+        if (cobalt.adapter.isBelowIOS7){
+            cobalt.adapter.ios6.send(obj);
+        }else{
+            if (obj && !cobalt.debugInBrowser){
+                cobalt.divLog('sending',obj)
+                try{
+                    CobaltWebCommunicationClass.handleMessageSentByJavaScript(JSON.stringify(obj));
+                }catch (e){
+                    cobalt.log('ERROR : cant connect to native.')
+                }
+
+            }
         }
     },
     //datePicker stuff
@@ -59,7 +59,43 @@ cobalt.ios_adapter={
         }
     },
 
-
+    ios6:{
+        // iOS < 7 is using an old-school url change hack to send messages from web to native.
+        // Because of the url change, only one message can be sent to the native at a time.
+        // The acquitement sent by native once each event has been received ensure this behavior.
+        // Messages are queued and sent one after the other as soon as the acq is received.
+        handleCallback:function(json){
+            switch(json.callback){
+                case "callbackSimpleAcquitment":
+                    cobalt.divLog("received message acquitement")
+                    cobalt.adapter.ios6.unpipe();
+                    if (cobalt.adapter.pipeline.length==0){
+                        cobalt.divLog('end of ios message stack')
+                        cobalt.adapter.pipelineRunning=false;
+                    }
+                    break;
+                default:
+                    cobalt.tryToCallCallback(json)
+                    break;
+            }
+        },
+        send:function(obj){
+            cobalt.divLog('adding to ios message stack', obj)
+            cobalt.adapter.pipeline.push(obj);
+            if (!cobalt.adapter.pipelineRunning){
+                cobalt.adapter.ios6.unpipe()
+            }
+        },
+        //unpipe elements when receiving a ACK from ios.
+        unpipe:function(){
+            cobalt.adapter.pipelineRunning=true;
+            var objToSend=cobalt.adapter.pipeline.shift();
+            if (objToSend && !cobalt.debugInBrowser){
+                cobalt.divLog('sending',objToSend)
+                document.location.href=encodeURIComponent("cob@l7#k&y"+JSON.stringify(objToSend));
+            }
+        }
+    },
 
     //default behaviours
     handleEvent : cobalt.defaultBehaviors.handleEvent,
