520451f44182e4e4ce6ec125ecef23445d13582a
Tuni <fehmi@haploid.fr>
2014-10-15 10:56:55 +0200

    fixed iToast display on ios 8 devices

---
+++ samples/CatalogApp/iOS/CatalogCobalt/CatalogCobalt/NativeBarsController.h
@@ -0,0 +1,13 @@
+//
+//  NativeBarsController.h
+//  CatalogCobalt
+//
+//  Created by Haploid on 14/10/14.
+//  Copyright (c) 2014 Haploid. All rights reserved.
+//
+
+#import "SimpleController.h"
+
+@interface NativeBarsController : SimpleController
+
+@end
---
+++ samples/CatalogApp/iOS/CatalogCobalt/CatalogCobalt/NativeBarsController.m
@@ -0,0 +1,22 @@
+//
+//  NativeBarsController.m
+//  CatalogCobalt
+//
+//  Created by Haploid on 14/10/14.
+//  Copyright (c) 2014 Haploid. All rights reserved.
+//
+
+#import "NativeBarsController.h"
+
+@interface NativeBarsController ()
+
+@end
+
+@implementation NativeBarsController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
