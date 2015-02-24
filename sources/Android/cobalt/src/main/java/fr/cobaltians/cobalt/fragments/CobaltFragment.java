247f406b52139b1e430a6c0ca08e04800834cd77
Sébastien Vitard <contact@sebastienvitard.name>
2015-02-24 17:46:15 +0100

    Fix WebView blink issue for Android < 4.1
    Just disable hardware acceleration…

--- sources/Android/cobalt/src/main/java/fr/cobaltians/cobalt/fragments/CobaltFragment.java
+++ sources/Android/cobalt/src/main/java/fr/cobaltians/cobalt/fragments/CobaltFragment.java
@@ -260,7 +260,7 @@ public abstract class CobaltFragment extends Fragment implements IScrollListener
 	}
 
     protected void setWebViewSettings(Object javascriptInterface) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) mWebView.setLayerType(View.LAYER_TYPE_HARDWARE ,null);
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) mWebView.setLayerType(View.LAYER_TYPE_HARDWARE ,null);
 
         mWebView.setScrollListener(this);
         mWebView.setScrollBarStyle(WebView.SCROLLBARS_INSIDE_OVERLAY);
