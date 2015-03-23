70f3cc26dc529b88da5fc85f15dd934604f0369f
Guillaume Gendre <dzb.rtz@gmail.com>
2015-03-23 14:58:53 +0100

    fixing path issue in distrib script

--- distribution/web/updateSamples.sh
+++ distribution/web/updateSamples.sh
@@ -5,5 +5,5 @@ cp Android/* ../../samples/HelloWorld/Android/Hello/assets/www/platform/
 cp iOs/* ../../samples/HelloWorld/iOs/Hello/www/platform/
 
 ##updating Catalog
-cp Android/* ../../samples/CatalogApp/Android/CobaltCatalog/assets/www/platform
+cp Android/* ../../samples/CatalogApp/Android/CobaltCatalog/app/src/main/assets/www/platform
 cp iOS/* ../../samples/CatalogApp/iOS/CatalogCobalt/CatalogCobalt/www/platform
